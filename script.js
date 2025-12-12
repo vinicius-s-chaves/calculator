8865a6591d232377a5ca8f169b4544b50512a75d
Vinicius Chaves <vinicius@Xubuntu.myguest.virtualbox.org>
2025-12-12 09:47:05 -0300

    Add printOperation function. This function checks if any button is pressed and prints it on the display

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ let firstNumber = 0;
 let secondNumber = 0;
 let operator = '';
 
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll(".button");
+
+// Check if any button is pressed
+buttons.forEach((button) => button.addEventListener("click", () => {
+    printOperation(button);
+}));
+
+// Prints the pressed button on the display
+function printOperation(button) {
+    let digit = document.createElement('h1');
+    digit.textContent = button.textContent;
+
+    display.appendChild(digit);
+};
+
 function operate(a, operator, b) {
     switch(operator) {
         case '+':
