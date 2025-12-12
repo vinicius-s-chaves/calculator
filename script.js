a585cb7a7f410f372068fefea6f347dfeacc04c5
Vinicius Chaves <vinicius@Xubuntu.myguest.virtualbox.org>
2025-12-12 08:28:55 -0300

    Add operate function. This recive two numbers and an operator, and than calls the function corresponding to the operator

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
+
+function operate(a, operator, b) {
+    switch(operator) {
+        case '+':
+            return add(a, b);
+            break;
+        case '-':
+            return subtract(a, b);
+            break;
+        case '*':
+            return multiply(a, b);
+            break;
+        case '/':
+            return divide(a, b);
+            break;
+        default:
+            "Not valid operation";
+            break;
+    };
+};
+
 function add(a, b) {
     return a + b;
 };
